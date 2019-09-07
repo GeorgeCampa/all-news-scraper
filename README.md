332cdaf4cf1b8a6c6d2ced373239ce96b4ae216a
George <Jc82200172@gmail.com>
2019-09-06 20:28:54 -0700

    2nd Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # all-news-scraper
+
+Create a web app that lets users view and leave comments on the latest news.
+
+Deployed to Heroku, with a set up in mLab provision.
+
+
+
+
+Runing npm packages:
+Run npm init
+express
+express-handlebars
+mongoose
+cheerio
+axios
--- server.js
+++ server.js
@@ -6,20 +6,30 @@ var express = require("express");
 var app = express();
 
 app.use(logger("dev"));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+app.use(bodyParser.urlencoded({ extended: false })
 );
 
 app.use(express.static(process.cwd() + "/public"));
 
 var exphbs = require("express-handlebars");
-app.engine("handlebars",exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//connecting to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+//mongoose.connect("mongodb://localhost/scraped_news");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to Mongoose!");
+});
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on PORT " + port);
 });
 
